5b7925a6313e1d9902fef0efd9693df2af86f565
Mr.Doe <mr.doe082@gmail.com>
2022-02-21 10:47:09 +0900

    회원 가입 까지

--- src/main/java/mrdoe/spring/service/MemberService.java
+++ src/main/java/mrdoe/spring/service/MemberService.java
@@ -4,6 +4,9 @@ import mrdoe.spring.domain.Member;
 import mrdoe.spring.repository.MemberRepository;
 import mrdoe.spring.repository.MemoryMemberRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public class MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
@@ -12,9 +15,29 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member) {
+
+        valiDateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
 
+    }
+
+    private void valiDateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
     }
 
 }
